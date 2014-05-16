b5b0237add7c951239f5d65d459bb0a12370a337
pbucci <pbucci@gmail.com>
2014-05-16 12:52:09 -0700

    test suite improved

--- modules/profiler.py
+++ modules/profiler.py
@@ -1,5 +1,3 @@
-import unittest
-
 # Text information for easy classification
 class TextMeta(object):
     def __init__(self,textpath):
@@ -26,34 +24,3 @@ class CharacterClass(object):
     def __init__(self,classname,charlist):
         self.charlist = charlist
         self.classname = classname
-
-class TestSuite(unittest.TestCase):
-
-    def setUp(self):
-        self.pathstring = "../texts/G1_Rujia_01_lunyu_seg_nopunc.txt"
-        self.textmeta = TextMeta(self.pathstring)
-
-    def testTextMetaFields(self):
-        self.assertEqual(self.textmeta.path, self.pathstring)
-
-    def testGenre(self):
-        self.assertEqual(self.textmeta.genre, 1)
-
-    def testSchool(self):
-        self.assertEqual(self.textmeta.school, "Rujia")
-
-    def testGenreNum(self):
-        self.assertEqual(self.textmeta.genreNum, 1)
-
-    def testName(self):
-        self.assertEqual(self.textmeta.name, "lunyu")
-
-    def testSeg(self):
-        self.assertEqual(self.textmeta.seg, "seg")
-
-    def testPunc(self):
-        self.assertEqual(self.textmeta.punc, "nopunc")
-
-
-if __name__ == '__main__':
-    unittest.main()
